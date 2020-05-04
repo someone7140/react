401a0dd7b7d5eb14a8115ca43bef0128d85bc50d
someone7140 <ti7140@mail.goo.ne.jp>
2020-05-04 20:27:33 +0900

    infoWindowの追加

--- delivery-view/src/components/map/GoogleMapComponent.js
+++ delivery-view/src/components/map/GoogleMapComponent.js
@@ -1,33 +1,59 @@
-import React, { useContext, useEffect, useRef } from "react";
-import { GoogleApiWrapper, Marker, Map } from "google-maps-react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { GoogleApiWrapper, InfoWindow, Marker, Map } from "google-maps-react";
 import { MapStore } from "../../store/MapStore";
 import { setGeocoder } from "../../service/GeolocationService";
 import { updateStore } from "../../service/StoreService";
 
+const mapStyle = {
+  width: "100%",
+  height: "85%",
+};
+
 export const GoogleMapComponent = ({ google }) => {
   const { state, dispatch } = useContext(MapStore);
+  const [infoWindowState, setInfoWindowState] = useState({
+    markerInfo: {},
+    visible: false,
+  });
   const location = state.currentLocation;
-  const mapStyle = {
-    width: "100%",
-    height: "85%",
-  };
   var markerList = [];
 
-  const mounted = useRef(false);
+  const onMarkerClick = (_, marker) => {
+    // markerListから該当のレコードを取得
+    const markerInfo = state.displayStoreList.find((m) => m.id === marker.name);
+    if (markerInfo) {
+      setInfoWindowState({
+        activeMarker: marker,
+        markerInfo: markerInfo,
+        visible: true,
+      });
+    }
+  };
+
+  function onInfoWindowClose() {
+    setInfoWindowState({
+      markerInfo: {},
+      visible: false,
+    });
+  }
+
+  const googleMounted = useRef(false);
   useEffect(() => {
-    if (!mounted.current) {
+    if (!googleMounted.current) {
       const geocoder = new google.maps.Geocoder();
       setGeocoder(dispatch, geocoder);
     }
-    mounted.current = true;
+    googleMounted.current = true;
   }, [google, dispatch]);
 
-  state.displayStoreList.map((s, i) => {
+  state.displayStoreList.map((s) => {
     return markerList.push(
       <Marker
-        key={i}
+        key={s.id}
+        name={s.id}
         title={s.name}
         label={s.name}
+        onClick={onMarkerClick}
         position={{ lat: s.latitude, lng: s.longitude }}
       />
     );
@@ -54,6 +80,47 @@ export const GoogleMapComponent = ({ google }) => {
       onDragend={centerMoved}
     >
       {markerList}
+      <InfoWindow
+        marker={infoWindowState.activeMarker}
+        onClose={onInfoWindowClose}
+        visible={infoWindowState.visible}
+      >
+        <div>
+          <a
+            href={infoWindowState.markerInfo.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {infoWindowState.markerInfo.name}
+          </a>
+          <br />
+          {infoWindowState.markerInfo.category}
+          <br />
+          {infoWindowState.markerInfo.image &&
+            infoWindowState.markerInfo.image.length > 0 && (
+              <img
+                src={infoWindowState.markerInfo.image}
+                alt={infoWindowState.markerInfo.name}
+              />
+            )}
+          <br />
+          {infoWindowState.markerInfo.type === "all" && (
+            <span>テイクアウト・デリバリー可</span>
+          )}
+          {infoWindowState.markerInfo.type === "deliverly" && (
+            <span>デリバリー可</span>
+          )}
+          {infoWindowState.markerInfo.type === "takeout" && (
+            <span>テイクアウト可</span>
+          )}
+          <br />
+          【営業日】{infoWindowState.markerInfo.opentime}
+          <br />
+          【休日】{infoWindowState.markerInfo.holiday}
+          <br />
+          {infoWindowState.markerInfo.pr}
+        </div>
+      </InfoWindow>
     </Map>
   );
 };
