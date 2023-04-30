b8fd9156ff180b241b0d97506707e4f9f258324b
someone7140 <ti7140@mail.goo.ne.jp>
2023-05-01 02:23:55 +0900

    更新

--- novel-setting-view/src/components/novelSetting/detail/input/NovelSettingDetailChildComponent.js
+++ novel-setting-view/src/components/novelSetting/detail/input/NovelSettingDetailChildComponent.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import { v4 as uuidv4 } from "uuid";
+
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 
@@ -12,19 +14,20 @@ export default function NovelSettingDetailChildComponent(prop) {
     setSettingValue(prop.setting.value);
   }, [prop.setting]);
 
-  const loopChildrenUpdate = (children, name, value) => {
+  const loopChildrenUpdate = (children, name, value, addChild) => {
     if (children && children.length > 0) {
       const updatedChildren = children.map((s) => {
         if (s._id != prop.setting._id) {
           return {
             ...s,
-            children: loopChildrenUpdate(s.children, name, value),
+            children: loopChildrenUpdate(s.children, name, value, addChild),
           };
         } else {
           return {
             ...s,
             name: name,
             value: value,
+            children: addChild ? [...s.children, addChild] : s.children,
           };
         }
       });
@@ -41,7 +44,7 @@ export default function NovelSettingDetailChildComponent(prop) {
       } else {
         return {
           ...s,
-          children: loopChildrenUpdate(s.children, name, value),
+          children: loopChildrenUpdate(s.children, name, value, undefined),
         };
       }
     });
@@ -82,42 +85,103 @@ export default function NovelSettingDetailChildComponent(prop) {
     prop.setSettings(deletedSettings);
   };
 
+  const addChildren = () => {
+    const addChild = {
+      _id: uuidv4(),
+      children: [],
+    };
+    const updatedSettings = prop.settings.map((s) => {
+      if (s._id != prop.rootId) {
+        return s;
+      } else {
+        return {
+          ...s,
+          children: loopChildrenUpdate(
+            s.children,
+            settingName,
+            settingValue,
+            addChild
+          ),
+        };
+      }
+    });
+    prop.setSettings(updatedSettings);
+  };
+
   return (
     <div
       style={{
         display: "flex",
-        gap: 30,
+        flexDirection: "column",
+        gap: 10,
       }}
     >
-      <InputText
-        key={`${prop.setting._id}childName`}
-        type="text"
-        placeholder="設定名を入力"
-        value={settingName}
-        onChange={(e) => {
-          changeNameAndValue(e.target.value, settingValue);
-        }}
-        style={{ width: 300 }}
-      />
-      <InputText
-        key={`${prop.setting._id}childValue`}
-        type="text"
-        placeholder="設定値を入力"
-        value={settingValue}
-        onChange={(e) => {
-          changeNameAndValue(settingName, e.target.value);
-        }}
-        style={{ width: 300 }}
-      />
-      <Button
-        rounded
-        severity="secondary"
-        onClick={() => {
-          deleteSetting();
+      <div
+        style={{
+          display: "flex",
+          gap: 30,
         }}
       >
-        削除
-      </Button>
+        <InputText
+          key={`${prop.setting._id}childName`}
+          type="text"
+          placeholder="設定名を入力"
+          value={settingName}
+          onChange={(e) => {
+            changeNameAndValue(e.target.value, settingValue);
+          }}
+          style={{ width: 300 }}
+        />
+        <InputText
+          key={`${prop.setting._id}childValue`}
+          type="text"
+          placeholder="設定値を入力"
+          value={settingValue}
+          onChange={(e) => {
+            changeNameAndValue(settingName, e.target.value);
+          }}
+          style={{ width: 300 }}
+        />
+        <Button
+          rounded
+          severity="info"
+          onClick={() => {
+            addChildren();
+          }}
+        >
+          子設定追加
+        </Button>
+        <Button
+          rounded
+          severity="secondary"
+          onClick={() => {
+            deleteSetting();
+          }}
+        >
+          削除
+        </Button>
+      </div>
+      {prop.setting?.children?.map((child) => {
+        return (
+          <div
+            key={child._id}
+            style={{
+              marginLeft: 40,
+              display: "flex",
+              flexDirection: "column",
+              gap: 10,
+            }}
+          >
+            <NovelSettingDetailChildComponent
+              key={`${child._id}component`}
+              setSettings={prop.setSettings}
+              setting={child}
+              settings={prop.settings}
+              rootId={prop.rootId}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
