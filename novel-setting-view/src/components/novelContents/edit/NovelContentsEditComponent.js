8cd477c727fa7ed0bd887e57957aee4c1a94699a
someone7140 <ti7140@mail.goo.ne.jp>
2023-05-12 03:15:57 +0900

    更新

--- novel-setting-view/src/components/novelContents/edit/NovelContentsEditComponent.js
+++ novel-setting-view/src/components/novelContents/edit/NovelContentsEditComponent.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+import { Button } from "primereact/button";
 import { Card } from "primereact/card";
-import { createEditor } from "slate";
+import { createEditor, Editor, Element as SlateElement } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 
 export default function NovelContentsEditComponent(prop) {
@@ -13,6 +14,27 @@ export default function NovelContentsEditComponent(prop) {
   ];
 
   const [editor] = useState(() => withReact(createEditor()));
+
+  const getBlockKey = (selection, format, blockType = "type") => {
+    if (!selection) return undefined;
+
+    const [match] = Array.from(
+      Editor.nodes(editor, {
+        at: Editor.unhangRange(editor, selection),
+        match: (n) =>
+          !Editor.isEditor(n) &&
+          SlateElement.isElement(n) &&
+          n[blockType] === format,
+      })
+    );
+    return match?.length ? match[0].key : undefined;
+  };
+
+  useEffect(() => {
+    const key = getBlockKey(editor.selection, "headline");
+    console.log(key);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [editor.selection]);
 
   return (
     <div
@@ -35,7 +57,26 @@ export default function NovelContentsEditComponent(prop) {
         <div style={{ wordWrap: "break-word" }}>aaaa</div>
       </Card>
       <Card
-        title="本文"
+        title={
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: 45,
+            }}
+          >
+            本文
+            <Button
+              severity="help"
+              style={{
+                height: 15,
+                width: 140,
+              }}
+            >
+              見出しの設定
+            </Button>
+          </div>
+        }
         style={{
           width: 1000,
           textAlign: "start",
